dea95c93d5e05abfa2c8074e775a834a1dedbd87
Santiago <acunia.santiago@gmail.com>
2017-05-27 14:22:25 -0300

    Add basic POST for raw data.

--- webapp.py
+++ webapp.py
@@ -1,14 +1,23 @@
 import os
+import json
 from slackclient import SlackClient
-from flask import Flask
+from flask import Flask, request
+from flask_pymongo import PyMongo
 from flask import render_template
 app = Flask(__name__)
+app.config['MONGO_DBNAME'] = 'zenoffice'
+mongo = PyMongo(app)
 slack_token = os.environ["SLACK_BOT_TOKEN"]
 sc = SlackClient(slack_token)
 
 @app.route("/")
-def hello():
+def dashboard():
     return render_template('dashboard.html')
+
+@app.route("/<year>/<month>/<day>")
+def day_tts_data():
+    """ Returns TTS data for given day """
+    return
 
 @app.route("/slack")
 def slack():
@@ -18,6 +27,18 @@ def slack():
       text="Shhhh! Estan en call!"
     )
     return "sent"
+
+
+@app.route("/add_ttss", methods=['POST'])
+def add_ttss():
+    insert_data = parse_data(request.get_json(silent=True))
+    mongo.db.ttss.insert_one(insert_data)
+    return json.dumps({'success': True}), 200, {'Content-Type': 'application/json'}
+
+def parse_data(raw_data):
+    # TODO transformar el timestamp en algo que podamos usar
+    # TODO parsear el volumen y la temperatura si hace falta
+    return raw_data
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
