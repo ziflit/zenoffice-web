a10608b226929c701331fde3ed8edc2dd4918c03
Santiago <acunia.santiago@gmail.com>
2017-05-27 19:35:37 -0300

    nose, anda loco ya fue

--- webapp.py
+++ webapp.py
@@ -1,10 +1,14 @@
 import os
 import json
+from bson.json_util import dumps
+import datetime
 from slackclient import SlackClient
-from flask import Flask, request
+from flask import Flask, request, jsonify
 from flask_pymongo import PyMongo
+from flask_cors import CORS,cross_origin
 from flask import render_template
 app = Flask(__name__)
+CORS(app)
 app.config['MONGO_DBNAME'] = 'zenoffice'
 mongo = PyMongo(app)
 slack_token = os.environ["SLACK_BOT_TOKEN"]
@@ -14,21 +18,24 @@ sc = SlackClient(slack_token)
 def dashboard():
     return render_template('dashboard.html')
 
-@app.route("/<year>/<month>/<day>")
+@app.route("/tts")
 def day_tts_data():
     """ Returns TTS data for given day """
-    return
+    ffrom = request.args.get('from')
+    if ffrom is None:
+        return jsonify(last_tts_data())
+    d = mongo.db.ttss.find({
+        "timestamp": {
+            "$gt": int(ffrom)
+        }
+    }, {'_id': False}).sort("timestamp")
+    str_data =  dumps(d)
+    return jsonify(str_data)
 
 @app.route("/slack")
 def slack():
-    sc.api_call(
-      "chat.postMessage",
-      channel="#general",
-      text="Shhhh! Estan en call!",
-      as_user=True
-    )
+    slack_message('sshh callense', '#general')
     return "sent"
-
 
 @app.route("/add_ttss", methods=['POST'])
 def add_ttss():
@@ -37,9 +44,22 @@ def add_ttss():
     return json.dumps({'success': True}), 200, {'Content-Type': 'application/json'}
 
 def parse_data(raw_data):
-    # TODO transformar el timestamp en algo que podamos usar
-    # TODO parsear el volumen y la temperatura si hace falta
+    raw_data['timestamp'] = datetime.datetime.now().strftime("%s")
     return raw_data
+
+def last_tts_data():
+    cursor = mongo.db.ttss.find({}, {'_id': False}).sort('timestamp', -1).limit(1)
+    return dumps(cursor)
+
+
+def slack_message(text, channel):
+    sc.api_call(
+      "chat.postMessage",
+      channel=channel,
+      text=text,
+      as_user=True
+    )
+    return None
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
