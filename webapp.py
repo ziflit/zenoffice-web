261406c93465adf50cd063f64c269cbaaff6859a
Pablo Ferro <pablo.pyp@gmail.com>
2017-05-27 12:22:46 -0300

    Example slack message

--- webapp.py
+++ webapp.py
@@ -1,9 +1,22 @@
+import os
+from slackclient import SlackClient
 from flask import Flask
 app = Flask(__name__)
+slack_token = os.environ["SLACK_BOT_TOKEN"]
+sc = SlackClient(slack_token)
 
 @app.route("/")
 def hello():
     return "Hello World!"
+
+@app.route("/slack")
+def slack():
+    sc.api_call(
+      "chat.postMessage",
+      channel="#general",
+      text="Shhhh! Estan en call!"
+    )
+    return "sent"
 
 if __name__ == "__main__":
     app.run()
